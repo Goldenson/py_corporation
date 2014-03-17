d87e4a42c6bc9c8cdd8d1457ec769bc37ba538ec
Maxime Orefice <orefice.maxime@gmail.com>
2014-03-17 21:40:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PyCorporation::Application.config.secret_key_base = '06562c3c8b66a0bf769a596e35b62c33f935a4cea1be7348a2d99288b6d5f079119f5860c08a1369b08d5cff658e835eb07acdb71f8e5b62233d3d2e70de3ceb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PyCorporation::Application.config.session_store :cookie_store, key: '_py_corporation_session'
